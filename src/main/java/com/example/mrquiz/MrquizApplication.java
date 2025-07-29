0d70805867755a267372c06c326b8111dc7dc0c1
ABDESSADEQMAKKIOUI <abdessadeq.elmakkioui.20@gmail.com>
2025-07-29 11:58:23 +0100

    first commit

---
+++ src/main/java/com/example/mrquiz/MrquizApplication.java
@@ -0,0 +1,13 @@
+package com.example.mrquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mrquiz/MrquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mrquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
